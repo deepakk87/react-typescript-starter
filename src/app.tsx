9c09c3d1eda711cd043579a034dd0d086ce46c24
Deepak Kumar <deepakk87@gmail.com>
2017-12-24 13:57:25 +0530

    Hello React JS

---
+++ src/app.tsx
@@ -0,0 +1,8 @@
+var template = <div className="main" id= "abdc" >
+      <h1>Main Heading</h1>
+      <p>This is the first paragraph</p>
+      <p>This is the second Paragraph</p>
+    </div>
+
+var root = document.getElementById("main");
+ReactDOM.render(template, root);

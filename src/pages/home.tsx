608123b285853e3544a34dc2d19eb9a3ea35d2f4
Deepak Kumar <deepakk87@gmail.com>
2018-01-24 20:48:28 +0530

    react-table

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,26 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-
+import ReactTable from "react-table";
+import "react-table/react-table.css";
 export class Home extends React.Component {
     public render() {
+        const data = [{
+            age: 26,
+            name: "Tanner Linsley",
+          }, {
+            age: 23,
+            name: "Jason Maurer",
+          }];
+
+        const columns = [{
+            Header: "Name",
+            accessor: "name", // String-based value accessors!
+          }, {
+            Cell: (props: any)  => <span className="number">{props.value}</span>, // Custom cell components!
+            Header: "Age",
+            accessor: "age",
+        }];
+
         return <div>
         <h1>Home</h1>
         <div className="Home">
@@ -11,6 +29,8 @@ export class Home extends React.Component {
            optio eius tenetur praesentium iste pariatur rem, suscipit qui
            eaque esse dignissimos.
         </div>
+        <ReactTable data={data}
+                    columns={columns}/>
     </div>;
     }
 }
